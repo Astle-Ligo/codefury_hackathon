c3d0929932a5a06683faff146dbac281cd734f58
Astle-Ligo <astlelijo04@gmail.com>
2023-09-23 18:15:37 +0530

    login signup page done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('user/index', { loggedIn: false });
+});
+router.get('/login', (req, res) => {
+  res.render('user/login', { loggedIn: false })
 });
 
+
+router.get('/signup', (req, res) => {
+  res.render('user/signup', { loggedIn: false })
+})
 module.exports = router;
